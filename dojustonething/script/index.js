a0c9dedb2869b1ba586bf1e96e84d335108bcc35
Joe Mathes <jmathes@gmail.com>
2013-01-08 22:53:59 -0800

    don't add new click methods to the compare buttons every time

--- dojustonething/script/index.js
+++ dojustonething/script/index.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-    var compare = function(newthing, benchmark) {
-        var draw_button = function(selector, newthing, benchmark,
-                                is_newthing_button) {
+    $("#newthing_task").click(function() {
+        window.newthing.lower_bound = window.benchmark.urgency;
+        window.api("addtask", [window.newthing], thing_added);
+        $("#compare_dialog").dialog("close");
+    });
+
+    $("#benchmark_task").click(function() {
+        window.newthing.upper_bound = window.benchmark.urgency;
+        window.api("addtask", [window.newthing], thing_added);
+        $("#compare_dialog").dialog("close");
+    });
+
+    var compare = function() {
+        var draw_button = function(selector, is_newthing_button) {
             if (is_newthing_button) {
-                label = newthing.task;
+                label = window.newthing.task;
             } else {
-                label = benchmark.task;
+                label = window.benchmark.task;
 
             }
             $(selector).button({
                 label: label,
                 disabled: false
             });
-            $(selector).click(function() {
-                if (is_newthing_button) {
-                    newthing.lower_bound = benchmark.urgency;
-                } else {
-                    newthing.upper_bound = benchmark.urgency;
-                }
-                window.api("addtask", [newthing], thing_added);
-                $("#compare_dialog").dialog("close");
-            });
-
         };
-        draw_button("#newthing_task", newthing, benchmark, true);
-        draw_button("#benchmark_task", newthing, benchmark, false);
+        draw_button("#newthing_task", true);
+        draw_button("#benchmark_task", false);
         $(".comparison").addClass("comparison");
         $("#compare_dialog").dialog("open");
     };
 
     var thing_added = function(response) {
-        console.log(response);
         if (response.success) {
             $("#thing-to-do").text(response.top_item);
         } else {
-            console.log(response);
-            compare(response.newthing, response.benchmark);
+            window.newthing = response.newthing;
+            window.benchmark = response.benchmark;
+            compare();
         }
     };
 
